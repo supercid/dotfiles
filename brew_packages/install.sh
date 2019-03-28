9132f8a647a48d69033bcfd22d852d9e6e9bb493
Cid Lopes <alannettto@gmail.com>
2019-03-28 11:41:52 +0200

    Install packages one by one.

--- brew_packages/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Reinstall all of my brew formulae
-xargs brew tap < $DOTFILES/brew_packages/tap_list.txt
-xargs brew install < $DOTFILES/brew_packages/brew_packages.txt
---
+++ dry/install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+brew tap moncho/dry
+brew install dry
