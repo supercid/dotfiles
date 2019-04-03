19d29e28708eb2304dc807d08180244d4f95d6a7
Cid Lopes <alannettto@gmail.com>
2019-04-03 13:28:06 +0300

    Add speedtest-cli

--- fonts/install.sh
+++ fonts/install.sh
@@ -3,7 +3,8 @@ URL="https://github.com/powerline/fonts/archive/2015-12-04.zip"
 install() {
 	curl -L -s -o /tmp/powerline.zip "$URL"
 	unzip -o /tmp/powerline.zip -d /tmp/PowerlineFonts
-	cp /tmp/PowerlineFonts/fonts-2015-12-04/*/*.ttf "$1"
+	cp /tmp/PowerlineFonts/fonts-2015-12-04/Meslo/*.otf "$1"
+	rm -rf /tmp/PowerlineFonts/fonts-2015-12-04
 }
 
 if [ "$(uname -s)" = "Darwin" ]; then
