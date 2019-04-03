1438bc6d63697aec37d7dc061bff2f3f8358f336
Cid Lopes <alannettto@gmail.com>
2019-04-03 10:48:24 +0300

    Check for antibody before installing

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if command -v brew >/dev/null 2>&1; then
 	brew tap | grep -q 'getantibody/tap' || brew tap getantibody/tap
-	brew install antibody
+	brew list | grep -q 'antibody' || brew install antibody
 else
 	curl -sL https://git.io/antibody | sh -s
 fi
