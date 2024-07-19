970fbf400b0a61fd4f5f52408d2b7bfbccbce646
Cid Lopes <alannettto@gmail.com>
2024-07-19 11:21:22 +0300

    Enable transient prompt in starship for fish

--- starship/conf.d/starship.fish
+++ starship/conf.d/starship.fish
@@ -1,4 +1,11 @@
 #!/usr/bin/env fish
 if command -q starship
+    function starship_transient_prompt_func
+        starship module character
+    end
+    function starship_transient_rprompt_func
+      starship module time
+    end
     starship init fish | source
+    enable_transience
 end
