9ae05e06af9c63bdf75adcd44ddab2f9db526fd4
Cid Lopes <alannettto@gmail.com>
2025-12-25 12:08:06 +0200

    SSH function now tries fish -> ZSH -> Bash instead of failing

--- fish/functions/ssh.fish
+++ fish/functions/ssh.fish
@@ -1,14 +1,16 @@
 function ssh
     switch (uname)
     case Darwin
-		set -x TERM xterm-256color
-	    /usr/bin/ssh -t "$argv" 'fish -c "
-		    if test -z \"$TMUX\"
-		        tmux attach || tmux new
-		    else
-		        $SHELL
-		    end"'
+	set -x TERM xterm-256color
+	/usr/bin/ssh -t "$argv" '
+	    if command -v fish >/dev/null 2>&1; then
+		fish -c "if test -z \"\$TMUX\"; tmux attach || tmux new; else; exec fish; end"
+	    elif command -v zsh >/dev/null 2>&1; then
+		zsh -c "if [ -z \"\$TMUX\" ]; then tmux attach || tmux new; else exec zsh; fi"
+	    else
+		bash -c "if [ -z \"\$TMUX\" ]; then tmux attach || tmux new; else exec bash; fi"
+	    fi'
     case '*'
-        /usr/bin/ssh $argv
+	/usr/bin/ssh $argv
     end
 end
