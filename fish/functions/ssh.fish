1b961eb551b98f14b4a30a3ab43268406c1c63c7
Cid Lopes <alannettto@gmail.com>
2025-12-25 17:29:47 +0200

    Revert fish ssh check

--- fish/functions/ssh.fish
+++ fish/functions/ssh.fish
@@ -1,15 +1,13 @@
 function ssh
     switch (uname)
     case Darwin
-	set -x TERM xterm-256color
-	/usr/bin/ssh -t "$argv" '
-	    if command -v fish >/dev/null 2>&1; then
-		fish -c "if test -z \"\$TMUX\"; tmux attach || tmux new; else; exec fish; end"
-	    elif command -v zsh >/dev/null 2>&1; then
-		zsh -c "if [ -z \"\$TMUX\" ]; then tmux attach || tmux new; else exec zsh; fi"
-	    else
-		bash -c "if [ -z \"\$TMUX\" ]; then tmux attach || tmux new; else exec bash; fi"
-	    fi'
+		set -x TERM xterm-256color
+	    /usr/bin/ssh -t "$argv" 'fish -c "
+		    if test -z \"$TMUX\"
+		        tmux attach || tmux new
+		    else
+		        $SHELL
+		    end"'
     case '*'
 	/usr/bin/ssh $argv
     end
