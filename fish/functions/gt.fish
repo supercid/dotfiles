a2955f4b8c3c59fa9c508ffb480b31ab17b91035
Cid Lopes <alannettto@gmail.com>
2026-07-29 10:55:46 +0300

    Add gt fish function to open current dir in Tower

---
+++ fish/functions/gt.fish
@@ -0,0 +1,6 @@
+function gt
+	# Open the current directory in Tower
+	# To always open in a new tab, run:
+	# defaults write com.fournova.Tower3 AppleWindowTabbingMode -string always
+	gittower -n .
+end
